e5bd5f702db737a632d78295c08224e469f1bca2
ppatil <ppatil@localhost>
2018-01-10 16:30:14 +0530

    commit updates

--- GitHubDemo/src/com/pravin/github/Test.java
+++ GitHubDemo/src/com/pravin/github/Test.java
@@ -3,5 +3,6 @@ package com.pravin.github;
 public class Test {
     public static void main(String[] args) {
 	System.out.println("Hi, Welcome to GitHub");
+	System.out.println("Done with this configuration");
     }
 }
